ef982b05c24d9b2cca8498a817691cba87a0f76d
Athura <joshualjohnson33@gmail.com>
2018-07-16 14:57:51 -0400

    implemented various changes to the parallax styles file

--- src/styles/_parallax.js
+++ src/styles/_parallax.js
@@ -14,23 +14,27 @@ export const StyledParallax = styled.div`
     background-attachment: fixed;
 
     ${props => props.primary && css`
-        background-image: url('/assets/PDJ_20180331_3637c.jpg');
-        height: 100vh;    
+        background-image: url('/assets/PDJ_20180331_3637c.jpg');    
     `};
 
     ${props => props.mountairy && css`
         background-image: url('/assets/MountAiry 3.jpg');
-        min-height: 400px;
     `}
 
     ${props => props.newwindsor && css`
         background-image: url('/assets/NewWindsor 1.jpg');
-        min-height: 400px;
     `}
 
     ${props => props.marston && css`
         background-image: url('/assets/Marston 1.jpg');
+    `};
+
+    ${props => props.fullscreen && css`
         height: 100vh;
+    `};
+
+    ${props => props.minheight && css`
+        min-height: 400px;
     `};
     
 `;
